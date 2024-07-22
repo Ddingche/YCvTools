83502bca3de59bc7d09a26786e46f8b8170682fe
叶纯 <ye971249@gmail.com>
2024-07-23 00:33:41 +0800

    feat: add base data structure

---
+++ schemas/train_dataset.py
@@ -0,0 +1,22 @@
+import os
+from YCvTools.schemas.base import BaseModel
+from pydantic import Field
+
+class DataSet(BaseModel):
+    image_dir: str = Field(..., description="Path to the directory containing the images")
+    xml_dir: str = Field(..., description="Path to the directory containing the xml files")
+    txt_dir: str = Field(..., description="Path to the directory containing the txt files")
+    random_seed: int = Field(42, description="Seed for random number generator")
+
+    @property
+    def data_nums(self):
+        return len(os.listdir(self.image_dir))
+
+    @property
+    def split_dataset(self,train_ratio: float = 0.8,val_ratio: float = 0.2):
+        """
+        划分数据集
+        """
+        assert train_ratio > 0
+        assert val_ratio > 0
+        
